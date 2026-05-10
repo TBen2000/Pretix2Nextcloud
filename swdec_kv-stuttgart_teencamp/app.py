5889f6c7a79932e58de86bf1c7a351064bcba5e9
TBen2000 <thorben.tg@gmx.de>
2026-05-10 16:19:25 +0200

    fix: handle string boolean values in dietary columns for accurate replacement

--- swdec_kv-stuttgart_teencamp/app.py
+++ swdec_kv-stuttgart_teencamp/app.py
@@ -119,6 +119,7 @@ class Dataframe:
         # replace "False" and "True in columns "Vegetarisch", "Laktosefrei" and "Glutenfrei" with "Nein" and "Ja"
         for col in ["Vegetarisch", "Laktosefrei", "Glutenfrei"]:
             df[col] = df[col].replace({False: "Nein", True: "Ja"})
+            df[col] = df[col].replace({"False": "Nein", "True": "Ja"})
         # add colum "Ernährung" based on "Vegetarisch", "Laktosefrei" and "Glutenfrei"
         df["Ernährung"] = df.apply(lambda row: ", ".join([col for col in ["Vegetarisch", "Laktosefrei", "Glutenfrei"] if row[col] == "Ja"]), axis=1)
         
